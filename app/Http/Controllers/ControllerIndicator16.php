02faa89bf448ee13eaaefd2001b3175380c17844
Carlos DIaz <ceduardodch@gmail.com>
2023-02-13 20:16:49 -0500

    CD

--- app/Http/Controllers/ControllerIndicator16.php
+++ app/Http/Controllers/ControllerIndicator16.php
@@ -15,18 +15,21 @@ class ControllerIndicator16 extends Controller
         $mindate = filter_var($request->mindate, FILTER_SANITIZE_STRING);
         $procedimiento = filter_var($request->procedimiento, FILTER_SANITIZE_STRING);
         
-        $indicador16 = Indicador16::select(
+        $detailindicador16_1 = Indicador16::select(
         DB::raw('json_build_object(yearmonth, json_agg(ROUND(monto_licitado,2))) as data'))
         ->groupBy('yearmonth')
         ->whereBetween('yearmonth', [$mindate, $maxdate])
         ->where('procedimiento', $procedimiento)
         ->get();
         $indicador16f=array();
-        foreach ($indicador16 as $key => $value) {
-            $value1 =  str_replace(array("{", "}", "\"", '"'), "", json_encode($value['data']));
+        foreach ($detailindicador16_1 as $key => $value) {
+            $value1 =  str_replace(array("{", "}", "\"","\\", '"'), "", json_encode($value['data']));
              array_push($indicador16f ,$value1);
         }
-        return $indicador16f;
+        $json['chartData'] = $indicador16f;
+        $indicador16_1 = json_encode($json,  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT); 
+
+        return $indicador16_1;
         
     }
 }
